c18f244e5883496f096a9709bd54c20d8c27cbf3
PapaYu <chaofan.yu@kapeixi.com>
2022-03-08 10:40:33 +0800

    feat: eeee

--- memory-server/src/main/java/com/nafapap/memory/config/SwaggerConfig.java
+++ memory-server/src/main/java/com/nafapap/memory/config/SwaggerConfig.java
@@ -15,7 +15,10 @@ import springfox.documentation.spring.web.plugins.Docket;
 /**
  * <p>Project: memory </p>
  * <p>Name: SwaggerConfig </p>
- * <p>Description: ${描述} </p>
+ * <p>Description:
+ * http://localhost:8085/memory/swagger-ui/index.html
+ * http://localhost:8085/memory/docs.html
+ * </p>
  * <p>Date: 2022/3/1 16:33 </p>
  * <p>Company: Cupshe Company Limited </p>
  *
@@ -28,13 +31,10 @@ public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
-        //swagger设置，基本信息，要解析的接口及路径等
         return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
                 .select()
-                //设置通过什么方式定位需要自动生成文档的接口，这里定位方法上的@ApiOperation注解
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-                //接口URI路径设置，any是全路径，也可以通过PathSelectors.regex()正则匹配
                 .paths(PathSelectors.any())
                 .build();
     }
@@ -45,9 +45,9 @@ public class SwaggerConfig {
      */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger3接口文档")
+                .title("Memory接口文档")
                 .description("如有疑问，请联系开发工程师")
-                .contact(new Contact("lixuelong", "https://www.cnblogs.com/lixuelong/", "lixuelong@aliyun.com"))
+                .contact(new Contact("papafan", "https://www.papafan.com/", "papafan2020@outlook.com"))
                 .version("1.0")
                 .build();
     }
