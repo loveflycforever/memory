2362f0c48818cbab787e1a57b5a2178855d3b9c8
PapaYu <chaofan.yu@kapeixi.com>
2022-06-20 02:47:33 +0800

    feat: 123

--- memory-source/memo/v1.sql
+++ memory-source/memo/v1.sql
@@ -93,14 +93,14 @@ CREATE TABLE `t_procure`
     `purchase_quantity` int(11)             NOT NULL DEFAULT '0' COMMENT '购买数量',
     `purchase_location` varchar(255)        NOT NULL DEFAULT '' COMMENT '购买地点',
     `purchase_datetime` varchar(50)         NOT NULL DEFAULT '' COMMENT '购买日期',
-    `price`             decimal(14, 4)      NOT NULL DEFAULT 0.0000 COMMENT '购买价格',
+    `price`             decimal(14, 4)      NOT NULL DEFAULT 0.0000 COMMENT '价格',
     `currency`          varchar(20)         NOT NULL DEFAULT '' COMMENT '货币',
     `china_yuan`        decimal(14, 4)      NOT NULL DEFAULT 0.0000 COMMENT '人民币',
 
     `hold`              int(11)             NOT NULL DEFAULT '0' COMMENT '实际可用总量',
     `unit`              varchar(10)         NOT NULL DEFAULT '' COMMENT '单位',
-    `planned_day`       int(11)             NOT NULL DEFAULT '0' COMMENT '计划使用日',
-    `closing_date`      varchar(50)         NOT NULL DEFAULT '' COMMENT '结束日期',
+    `plan_day`       int(11)             NOT NULL DEFAULT '0' COMMENT '计划使用日',
+    `closed_date`      varchar(50)         NOT NULL DEFAULT '' COMMENT '结束日期',
     `actual_day`        int(11)             NOT NULL DEFAULT '0' COMMENT '实际天数',
 
     `creator_id`        bigint(20)          NOT NULL DEFAULT '0' COMMENT '创建人',
