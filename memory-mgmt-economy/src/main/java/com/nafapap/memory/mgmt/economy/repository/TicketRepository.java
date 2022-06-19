5469ffe540353980529fb64d0d3744afbac2a7e7
PapaYu <chaofan.yu@kapeixi.com>
2022-06-20 00:40:21 +0800

    feat: 123

--- memory-mgmt-economy/src/main/java/com/nafapap/memory/mgmt/economy/repository/TicketRepository.java
+++ memory-mgmt-economy/src/main/java/com/nafapap/memory/mgmt/economy/repository/TicketRepository.java
@@ -2,13 +2,12 @@ package com.nafapap.memory.mgmt.economy.repository;
 
 import cn.org.atool.fluent.mybatis.model.StdPagedList;
 import com.nafapap.memory.mgmt.economy.transobj.PageDto;
-import com.nafapap.memory.source.entity.SubjectEntity;
 import com.nafapap.memory.source.entity.TicketEntity;
-import com.nafapap.memory.source.mapper.SubjectMapper;
+import com.nafapap.memory.source.helper.TicketSegment;
 import com.nafapap.memory.source.mapper.TicketMapper;
-import com.nafapap.memory.source.wrapper.SubjectQuery;
 import com.nafapap.memory.source.wrapper.TicketQuery;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -29,9 +28,13 @@ public class TicketRepository {
     private final TicketMapper fmTicketMapper;
 
     public List<TicketEntity> select(PageDto dto) {
-        StdPagedList<TicketEntity> list = fmTicketMapper.stdPagedEntity(new TicketQuery()
-                .where.deleteFlag().isFalse().end()
-                .orderBy.purchaseDatetime().desc().end()
+        TicketSegment.QueryWhere queryWhere = new TicketQuery().where.deleteFlag().isFalse();
+        if(StringUtils.isNotBlank(dto.getTakingNo())) {
+            queryWhere.and.serialNo().eq(dto.getTakingNo());
+        }
+        TicketQuery query = queryWhere.end();
+        StdPagedList<TicketEntity> list = fmTicketMapper.stdPagedEntity(
+                query.orderBy.purchaseDatetime().desc().end()
                 .limit(dto.gainFrom(), dto.gainLimit())
         );
 
