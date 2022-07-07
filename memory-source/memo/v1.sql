73fdc92edc101a8d2e0fe096027dfe2c32cd28c6
PapaYu <chaofan.yu@kapeixi.com>
2022-07-07 17:12:55 +0800

    feat: 123

--- memory-source/memo/v1.sql
+++ memory-source/memo/v1.sql
@@ -57,6 +57,32 @@ CREATE TABLE `t_thing`
   AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4 COMMENT ='事物表';
 
+CREATE TABLE `t_thing_check`
+(
+    `id`            bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '唯一编号',
+    `serial_no`     varchar(100)        NOT NULL DEFAULT '' COMMENT '序列号',
+
+    `x_thing`       varchar(50)         NOT NULL DEFAULT '' COMMENT '事物',
+    `quantity`      decimal(14, 4)      NOT NULL DEFAULT 0.0000 COMMENT '数量',
+    `specific`      varchar(10)         NOT NULL DEFAULT '' COMMENT '单位',
+
+    `creator_id`    bigint(20)          NOT NULL DEFAULT '0' COMMENT '创建人',
+    `creator_name`  varchar(64)         NOT NULL DEFAULT '' COMMENT '创建人名称',
+    `create_time`   timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `status`        varchar(50)         NOT NULL DEFAULT '' COMMENT '状态',
+
+    `remark`        varchar(255)        NOT NULL DEFAULT '' COMMENT '备注',
+    `delete_flag`   tinyint(2)          NOT NULL DEFAULT '0' COMMENT '作废标志 0 未作废 1作废',
+    `operator_id`   bigint(20)          NOT NULL DEFAULT '0' COMMENT '操作人',
+    `operator_name` varchar(64)         NOT NULL DEFAULT '' COMMENT '操作人名称',
+    `operate_time`  timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '操作时间',
+    `version`       int(11)             NOT NULL DEFAULT '0' COMMENT '版本',
+    PRIMARY KEY (`id`) USING BTREE,
+    KEY `operate_sort` (`operate_time`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4 COMMENT ='事物盘点表';
+
 CREATE TABLE `t_goods`
 (
     `id`              bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '唯一编号',
